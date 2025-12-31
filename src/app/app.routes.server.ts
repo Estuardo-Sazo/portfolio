d5ae40462148033e879442407e91213a408264ce
Jaime Estuardo Sazo <estuardosazo1206@gmail.com>
2025-12-30 21:35:56 -0600

    feat: Add project data and service, and implement server-side prerendering for project detail pages.

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,8 +1,16 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { PROJECTS_DATA } from './core/services/projects';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'projects/:id',
+    renderMode: RenderMode.Prerender,
+    getPrerenderParams: async () => {
+      return PROJECTS_DATA.map((project) => ({ id: project.id! }));
+    },
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
